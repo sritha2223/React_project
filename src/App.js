a724f3930d3e18af99a7a76264b4fcac0bf7ad39
sritha2223 <ragasritha71@gmail.com>
2025-06-30 11:51:24 +0530

    Show and Hide Text

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import React,{ useState } from 'react'
 
 function App() {
   const[darkMode,setDarkMode]=useState(false);
+  const [inputText, setInputText] = useState("");
+  const[visible, setVisible] = useState(false);
   return (
    <div className={darkMode ? "container dark":"container"}>
     <h1>Click Counter App</h1>
     <Counter/>
-    <button className="btn" onClick={()=>setDarkMode(!darkMode)}>
-      {darkMode ? "Switch to LightMode" :"Switch to darkmode"}
-    </button>
+
+
+    <button className="btn show-btn" onClick={() => setVisible(true)}>Show Message</button>
+    <button className="btn hide-btn" onClick={() => setVisible(false)}>Hide Message</button>
+    {visible && <p className="hidden-text">🌟 This is a special secret message just for you! 🌟</p>}
+
+    <button className="theme-toggle-btn" onClick={() => setDarkMode(!darkMode)}>
+  {darkMode ? "☀ Light Mode" : "🌙 Dark Mode"}
+</button>
+
    </div>
   );
 }
